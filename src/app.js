5d0c7377d831aed62d3931ba90c2e66d69dc8b55
Kevin Villafuerte Montero <116960904+Kevin962022@users.noreply.github.com>
2024-03-14 08:54:09 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let randomNumber = Math.random() * 10; // creamos una variable para guardar el numero aleatorio - a
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hello Rigo from the console!" + randomNumber);
 };
